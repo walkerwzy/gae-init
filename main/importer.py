ff2ada250a460612a7c09fd18f68b48a71560ddb
walker <walker.wzy@gmail.com>
2014-05-25 19:17:24 +0800

    remove blog merge codes

--- main/importer.py
+++ main/importer.py
@@ -22,61 +22,62 @@ from datetime import datetime
 from collections import Counter
 
 # @app.route('/im')
-@auth.admin_required
-def im():
-	# import all data
-	# dir = os.path.dirname(os.path.abspath(__file__))
-	# arts = []
-	# for i in range(1,6):
-	# 	filename = 'importer/0%d.xml'%i
-	# 	filename = os.path.join(dir,filename)
-	# 	dom = parse(filename)
-	# 	for item in dom.getElementsByTagName('item'):
-	# 		c = getNodeValue(item,'full')
-	# 		arts.append(cms.Article(
-	# 			author=auth.current_user_key(),
-	# 			category=getCategory(getNodeValue(item,'category')),
-	# 			title=getNodeValue(item,'title'),
-	# 			abstract=util.remove_html_markup(c)[:200],
-	# 			content=c,
-	# 			tags=getNodeValue(item,'tags').split(','),
-	# 			created=datetime.strptime(getNodeValue(item,'pubDate'),'%a, %d %b %Y %H:%M:%S GMT')
-	# 			))
-	# ndb.put_multi(arts)
-	# return response('done')
-	# recoculate tag and cate's entrycount
-	query = cms.Article.query().fetch(projection=['category','tags'])
-	cates = []
-	tags = []
-	for item in query:
-		cates.append(item.category)
-		tags+=item.tags
-	c1 = Counter(cates)
-	c2 = Counter(tags)
-	# logging.warning(c1)
-	# logging.warning(c2)
-	cs = []
-	ts = []
-	for k,v in c1.items():
-		category = k.get()
-		category.entrycount = v
-		cs.append(category)
-	for k,v in c2.items():
-		tag = cms.Tag(name = k, entrycount = v)
-		ts.append(tag)
-	ndb.put_multi(cs)
-	ndb.put_multi(ts)
-	return response('done')
+# @auth.admin_required
+# def im():
+# 	# import all data
+# 	dir = os.path.dirname(os.path.abspath(__file__))
+# 	arts = []
+# 	for i in range(1,6):
+# 		filename = 'importer/0%d.xml'%i
+# 		filename = os.path.join(dir,filename)
+# 		dom = parse(filename)
+# 		for item in dom.getElementsByTagName('item'):
+# 			c = getNodeValue(item,'full')
+# 			arts.append(cms.Article(
+# 				author=auth.current_user_key(),
+# 				category=getCategory(getNodeValue(item,'category')),
+# 				title=getNodeValue(item,'title'),
+# 				abstract=util.remove_html_markup(c)[:200],
+# 				content=c,
+# 				tags=getNodeValue(item,'tags').split(','),
+# 				created=datetime.strptime(getNodeValue(item,'pubDate'),'%a, %d %b %Y %H:%M:%S GMT')
+# 				))
+# 	ndb.put_multi(arts)
+# 	return response('done')
+	
+# 	# recoculate tag and cate's entrycount
+# 	query = cms.Article.query().fetch(projection=['category','tags'])
+# 	cates = []
+# 	tags = []
+# 	for item in query:
+# 		cates.append(item.category)
+# 		tags+=item.tags
+# 	c1 = Counter(cates)
+# 	c2 = Counter(tags)
+# 	# logging.warning(c1)
+# 	# logging.warning(c2)
+# 	cs = []
+# 	ts = []
+# 	for k,v in c1.items():
+# 		category = k.get()
+# 		category.entrycount = v
+# 		cs.append(category)
+# 	for k,v in c2.items():
+# 		tag = cms.Tag(name = k, entrycount = v)
+# 		ts.append(tag)
+# 	ndb.put_multi(cs)
+# 	ndb.put_multi(ts)
+# 	return response('done')
 
-def getNodeValue(node,name):
-	node = node.getElementsByTagName(name)[0]
-	if not node:
-		return ""
-	logging.warning('%s,%s'%(name,node.firstChild))
-	try:
-		return node.firstChild.nodeValue
-	except:
-		return ''
+# def getNodeValue(node,name):
+# 	node = node.getElementsByTagName(name)[0]
+# 	if not node:
+# 		return ""
+# 	logging.warning('%s,%s'%(name,node.firstChild))
+# 	try:
+# 		return node.firstChild.nodeValue
+# 	except:
+# 		return ''
 
 def getCategory(name):
 	if not name:
